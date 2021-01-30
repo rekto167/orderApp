c265c0bf59678fc3a7a7fc3f57c05e4ce758d1e4
rekto167 <ekysr99@gmail.com>
2021-01-30 12:18:07 +0700

    pertama

---
+++ orderApp/urls.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from django.urls import path, include
+
+from .views import IndexView, AboutView
+
+urlpatterns = [
+    # path('restoran/', include('restoran.urls', namespace='restoran')),
+    path('', IndexView.as_view(), name='home'),
+    path('about/', AboutView.as_view(), name='about'),
+    path('admin/', admin.site.urls),
+]
---
+++ restoran/urls.py
@@ -0,0 +1,6 @@
+# from django.urls import path
+# app_name = 'restoran'
+# urlpatterns = [
+#     path('', )
+# ]
+# #
